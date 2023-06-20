ab01128b8188ccdf0608c602c02efd45584c3be2
Allan Mwaura <allanmooker22@gmail.com>
2023-06-20 04:56:06 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+
+#include "main.h"
+
+
+	/**
+	 * print_alphabet - print all alphabet in lowercase
+	 * Written by Allan Mwaura
+	 */
+
+
+	void print_alphabet(void)
+	{
+		char letter;
+
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+
+		_putchar('\n');
+	}
+
