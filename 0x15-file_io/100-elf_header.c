58eb82d97cbc98e55593f010b3dba5b26182d971
Allan Mwaura <allanmooker22@gmail.com>
2023-08-08 08:43:07 -0700

    0x15. C - File I/O

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,6 +1,6 @@
 /*
  * File: 100-elf_header.c
- * Auth: Brennan D Baraban
+ * Auth: Allan Mwaura
  */
 
 #include <elf.h>
@@ -316,7 +316,4 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	free(header);
 	close_elf(o);
 	return (0);
-
-
-/*Written By : Allan Mwaura*/
 }
